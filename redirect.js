dd459dd4d1be81fc7cc026736b35369394b43dd4
anneidan1 <165968988+anneidan1@users.noreply.github.com>
2024-05-10 11:08:36 -0400

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,7 +1,5 @@
-// redirect.js
-
 module.exports = (req, res) => {
   // Redirect to the desired URL
-  res.writeHead(301, { Location: 'https://sappxfinityprofile.net/?suite' });
+  res.writeHead(301, { Location: 'https://example.com' });
   res.end();
 };
